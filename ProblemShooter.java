cfdeaed14e5485a2c0f9fc967f1133b90e176b86
2ndBrigadeWebProgrammer <kyungmin.choi@gistory.me>
2021-01-11 13:15:47 +0000

    implemented chooseSmallIndex function

--- ProblemShooter.java
+++ ProblemShooter.java
@@ -1,5 +1,9 @@
+import java.util.*;
+import java.io.*;
+
 public class ProblemShooter {
     public static void main (String [] args) {
+        
         String fileName="";
         ArrayList <Problem> problems;
         int [] indexes;
@@ -8,22 +12,44 @@ public class ProblemShooter {
         int score;
         
         fileName=fileChoose();
-        indexes=chooseSmallIndex();
-        problems = readEachProblem(NUMBER, indexes, fileName);
-        turnIn = exam(problems);
-        score = evaluate(turnIn);
+        indexes=chooseSmallIndex(NUMBER);
+        //problems = readEachProblem(indexes, fileName);
+        //turnIn = exam(problems);
+        //score = evaluate(turnIn);
         
-        System.out.println("The Score is : "+score);
+        //System.out.println("The Score is : "+score);
+        
+        
     }
     
     public static String fileChoose() {
-        String fileName = "";
+        String fileName = "chapters.txt"; //chapters.txt has the number of smallIndexes for each 
         return fileName;
     }
     
-    public static int[] chooseSmallIndex() { 
-        int[] result=new int[]{0,0,0};
+    public static int[] chooseSmallIndex(int count) { //finished
+        int[] result;
+        int [] chapterNums = new int [40];
+        
+        try {
+            Scanner scan = new Scanner(new File("chapters.txt"));
+            int flag=0;
+             while(scan.hasNextLine()){
+                 chapterNums[flag++] = Integer.parseInt(scan.nextLine());
+             }
+
+            result = new int[flag];
+                for(int i=0; i<result.length; i++) {
+                    System.out.println(chapterNums[i]);
+                    result[i]= (int)(Math.random()*chapterNums[i]+1);    
+                }
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+            result = new int[]{-1,-1,-1,-1};
+        }
+        catch (Exception e1) {e1.printStackTrace(); result = new int[]{-1,-1,-1,-1};}
         return result;
+
     }
     
     public static ArrayList<Problem> readEachProblem(int number, int[] indexes, String fileName) {
@@ -32,9 +58,11 @@ public class ProblemShooter {
         for (int i=0;i<number;i++) {
             
         }
+        
+        return result;
     }
     
-    public static boolean[] exam(ArrayList<problem> problems) {
+    public static boolean[] exam(ArrayList<Problem> problems) {
         boolean[] result = {true,true};
         return result;
     }

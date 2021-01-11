9650bda8fd1ac889f39d80b38cc3b2daccb7dfc6
2ndBrigadeWebProgrammer <kyungmin.choi@gistory.me>
2021-01-11 14:02:48 +0000

    implementing chooseSmallIndex

--- ProblemShooter.java
+++ ProblemShooter.java
@@ -10,10 +10,10 @@ public class ProblemShooter {
         final int NUMBER=40;
         boolean [] turnIn;
         int score;
-        
-        fileName=fileChoose();
+        fileName="chapters.txt";
         indexes=chooseSmallIndex(NUMBER);
-        //problems = readEachProblem(indexes, fileName);
+        
+        //problems = readEachProblem(indexes);
         //turnIn = exam(problems);
         //score = evaluate(turnIn);
         
@@ -21,42 +21,65 @@ public class ProblemShooter {
         
         
     }
-    
-    public static String fileChoose() {
-        String fileName = "chapters.txt"; //chapters.txt has the number of smallIndexes for each 
-        return fileName;
-    }
-    
-    public static int[] chooseSmallIndex(int count) { //finished
-        int[] result;
-        int [] chapterNums = new int [40];
+    public static String[] readFile(String filename) throws Exception {
+        String[] temp = new String [100];
         
-        try {
             Scanner scan = new Scanner(new File("chapters.txt"));
             int flag=0;
              while(scan.hasNextLine()){
-                 chapterNums[flag++] = Integer.parseInt(scan.nextLine());
+                 temp[flag++] = scan.nextLine();
              }
+        System.out.println("flag : "+flag);
+        String [] result = new String[flag];
+        for(int i=0; i<result.length;i++) result[i]=temp[i];
+        
+        return result;
+    }
+    
+    public static int[] chooseSmallIndex(int count) { 
+        int[] result;
+        int [] chapterNums = new int [40];
+        String []  chapters;
 
-            result = new int[flag];
+        try { 
+            chapters = readFile("chapters.txt"); 
+            //for(int i=0; i<chapters.length;i++) System.out.println("chapters["+i+"] : "+chapters[i]);
+            result = new int[count];
                 for(int i=0; i<result.length; i++) {
-                    System.out.println(chapterNums[i]);
+                    //System.out.println(chapterNums[i]);
                     result[i]= (int)(Math.random()*chapterNums[i]+1);    
                 }
-        } catch (FileNotFoundException e) {
+        
+        }  
+        catch (FileNotFoundException e) {
             System.out.println("File Not Found");
             result = new int[]{-1,-1,-1,-1};
         }
         catch (Exception e1) {e1.printStackTrace(); result = new int[]{-1,-1,-1,-1};}
         return result;
-
     }
     
-    public static ArrayList<Problem> readEachProblem(int number, int[] indexes, String fileName) {
-        ArrayList<Problem> result= new ArrayList<Problem>();
+    public static ArrayList<Integer> chooseNumbers(int range, int count) {
+        ArrayList<Integer> result = new ArrayList<Integer>(count);
+        int flag =0;
+        int temp;
+        while(flag<count) {
+            temp = new Integer((int)(Math.random()*range +1));
+            if(result.contains(temp)) ;
+            else { result.add(temp); flag++;}
+        }
         
-        for (int i=0;i<number;i++) {
-            
+        return result;
+    }
+    
+    
+    public static ArrayList<Problem> readEachProblem(int[] indexes) {
+        ArrayList<Problem> result= new ArrayList<Problem>();
+        Problem newProb;
+        
+        for (int i=0;i<40;i++) {
+            newProb = new Problem();
+            result.add(newProb);
         }
         
         return result;
